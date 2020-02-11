3b195511b0a698fa8229a288a3e7e863bd2c9eea
Liam Han <liamhan0905@gmail.com>
2020-02-10 21:53:22 -0800

    finished move method

--- turtlesim_cleaner.py
+++ turtlesim_cleaner.py
@@ -21,26 +21,37 @@ def poseCallBack(pose_message):
     yaw = pose_message.theta
 
 
-def move(speed, distance):
+def move(speed, distance, isForward):
 
-    vel_msg = Twist()
+    vel_msg = Twist()  # creating an instance of Twist message type
 
-    x0 = x
-    y0 = y
-    distance_moved = 0.0
+    if (isForward):
+        vel_msg.linear.x = abs(speed)
+    else:
+        vel_msg.linear.x = -abs(speed)
 
-    vel_msg.linear.x = speed
+    # initializing parameters
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    distance_traveled = 0.0
 
-    loop_rate = rospy.Rate(10)
-    vel_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    t0 = rospy.get_time()
+    loop_rate = rospy.Rate(100)
 
     while True:
         rospy.loginfo("Turtlesim moving forward")
         vel_publisher.publish(vel_msg)
+        t1 = rospy.get_time()
+
+        distance_traveled = speed * (t1 - t0)
+        rospy.spin
         loop_rate.sleep()
 
-        print distance_moved
-        if (distance_moved > distance):
+        print distance_traveled
+        if (distance_traveled > distance):
             rospy.loginfo("Reached")
             break
 
@@ -59,7 +70,8 @@ def Reset():
 if __name__ == "__main__":
 
     rospy.init_node('turtlesim_move_node', anonymous=True)
+    vel_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     pose_subscriber = rospy.Subscriber("/turtle1/pose", Pose, poseCallBack)
     time.sleep(2)
-    move(1, 5)
+    move(2, 5, False)
     Reset()
