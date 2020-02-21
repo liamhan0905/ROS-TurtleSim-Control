991c5aa79464476a815d421f7abd570394d25a1a
Liam Han <liamhan0905@gmail.com>
2020-02-21 00:31:29 -0800

    tested

--- turtlesim_cleaner.py
+++ turtlesim_cleaner.py
@@ -21,7 +21,9 @@ def poseCallBack(pose_message):
 
 
 def move(linear_speed, desired_distance, isForward):
-    global x
+    global x, y
+    x0 = x
+    y0 = y
     vel_msg = Twist()  # creating an instance of Twist message type
 
     if (isForward):
@@ -41,15 +43,16 @@ def move(linear_speed, desired_distance, isForward):
     loop_rate = rospy.Rate(100)
 
     while True:
-        rospy.loginfo("Turtlesim moving forward")
+        # rospy.loginfo("Turtlesim moving forward")
         vel_publisher.publish(vel_msg)
         t1 = rospy.Time.now().to_sec()
 
+        # traveled_distance + abs(0.5 * math.sqrt(((x-x0) ** 2) + ((y-y0) ** 2)))
         traveled_distance = linear_speed * (t1 - t0)
-        rospy.spin
-        loop_rate.sleep()
+        # rospy.spin
+        # loop_rate.sleep()
 
-        print traveled_distance
+        print "distance: {}, x: {}, y: {}, theta: {}".format(traveled_distance, x, y, theta)
         if (traveled_distance > desired_distance):
             rospy.loginfo("Reached")
             break
@@ -63,9 +66,9 @@ def rotate(angular_speed_rad, desired_angle, isClockwise):
     vel_msg = Twist()  # creating an instance of Twist message type
 
     if (isClockwise):
-        vel_msg.angular.z = abs(angular_speed_rad)
-    else:
         vel_msg.angular.z = -abs(angular_speed_rad)
+    else:
+        vel_msg.angular.z = abs(angular_speed_rad)
 
     # initializing parameters
     vel_msg.linear.x = 0
@@ -88,7 +91,7 @@ def rotate(angular_speed_rad, desired_angle, isClockwise):
         loop_rate.sleep()
 
         print radiansToDegree(traveled_angle)
-        if (radiansToDegree(traveled_angle) > desired_angle):
+        if (traveled_angle > desired_angle):
             rospy.loginfo("Reached")
             break
 
@@ -96,7 +99,7 @@ def rotate(angular_speed_rad, desired_angle, isClockwise):
     vel_publisher.publish(vel_msg)
 
 
-def moveToGoal(goal_pose_x, goal_pose_y, distance_tolerance):
+def moveToGoal(goal_pose, distance_tolerance):
     global x, y, theta
     vel_msg = Twist()  # creating an instance of Twist message type
     loop_rate = rospy.Rate(100)
@@ -105,7 +108,7 @@ def moveToGoal(goal_pose_x, goal_pose_y, distance_tolerance):
     kp_angular = 4
 
     while True:
-        distance = math.sqrt((x-goal_pose_x)**2 + (y-goal_pose_y)**2)
+        distance = math.sqrt((x-goal_pose.x)**2 + (y-goal_pose.y)**2)
         vel_msg.linear.x = kp_linear * distance
 
         vel_msg.linear.y = 0
@@ -113,12 +116,12 @@ def moveToGoal(goal_pose_x, goal_pose_y, distance_tolerance):
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
 
-        angle = math.atan2(goal_pose_y - y, goal_pose_x - x)
+        angle = math.atan2(goal_pose.y - y, goal_pose.x - x)
         vel_msg.angular.z = kp_angular * (angle - theta)
 
         vel_publisher.publish(vel_msg)
 
-        print "distance: {}, x: {}, y: {}".format(distance, x, y)
+        print "distance: {}, x: {}, y: {}, theta: {}".format(distance, x, y, theta)
         if (distance < distance_tolerance):
             print("reached goal")
             break
@@ -141,7 +144,7 @@ def setDesiredOrientation(angle_rad_desired):
         clockwise = True
     else:
         clockwise = False
-    rotate(degreesToRadians(50), math.degrees(abs(angle_rad_diff)), clockwise)
+    rotate(degreesToRadians(50), angle_rad_diff, clockwise)
 
 
 def Reset():
@@ -152,6 +155,54 @@ def Reset():
     rospy.spin()
 
 
+def gridClean():
+    pose = Pose()
+    pose.x = 1
+    pose.y = 1
+    pose.theta = 0
+    moveToGoal(pose, 0.7)
+    setDesiredOrientation(degreesToRadians(pose.theta))
+
+    print("here")
+    move(2.0, 9.0, True)
+    print("2nd")
+    rotate(degreesToRadians(20), degreesToRadians(90), False)
+    print("3rd")
+    move(2.0, 9.0, True)
+    rotate(degreesToRadians(20), degreesToRadians(90), False)
+    move(2.0, 1.0, True)
+    rotate(degreesToRadians(20), degreesToRadians(90), False)
+    move(2.0, 9.0, True)
+    rotate(degreesToRadians(20), degreesToRadians(90), True)
+    move(2.0, 1.0, True)
+    rotate(degreesToRadians(20), degreesToRadians(90), True)
+    move(2.0, 9.0, True)
+    pass
+
+
+def spiralClean():
+    global x, y
+    vel_msg = Twist()
+    loop_rate = rospy.Rate(1)
+    wk = 4
+    rk = 0
+
+    while((x < 10.5) and (y < 10.5)):
+        rk = rk+1
+        vel_msg.linear.x = rk
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = wk
+        vel_publisher.publish(vel_msg)
+        loop_rate.sleep()
+
+    vel_msg.linear.x = 0
+    vel_msg.angular.z = 0
+    vel_publisher.publish(vel_msg)
+
+
 if __name__ == "__main__":
 
     rospy.init_node('turtlesim_move_node', anonymous=True)
@@ -159,14 +210,8 @@ if __name__ == "__main__":
     pose_subscriber = rospy.Subscriber("/turtle1/pose", Pose, poseCallBack)
     time.sleep(2)
 
-    # testing move method
-    # move(2, 5, False)
+    # gridClean()
 
-    # testing rotate method
-    # rotate(1, 180, True)
+    spiralClean()
 
-    # testing movetogoal method
-    # moveToGoal(1, 1, 0.5)
-    rotate(degreesToRadians(30), 90, True)
-    # setDesiredOrientation(degreesToRadians(90))
     Reset()
